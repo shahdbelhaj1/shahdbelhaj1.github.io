168638cbfe8cbb4a1ced7657cc8b5caccbfa8c2f
shahdbelhaj1 <shahdfbelhaj@gmail.com>
2022-01-23 11:45:58 +0200

    v7

---
+++ forms/contactform.php
@@ -0,0 +1,49 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$company = $_POST['company'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = $email; 
+		
+		// WARNING: Be sure to change this. This is the address that the email will be sent to
+		$to = 'shahdfbelhaj@gmail.com'; 
+		
+		$subject = "Message from ".$name." ";
+		
+		$body = "From: $name\n E-Mail: $email\n Company: $company\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+
+		//Check if company name has been entered
+		if (!$_POST['company']) {
+			$errCompany = 'Please enter your company name';
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You!</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
+?>
